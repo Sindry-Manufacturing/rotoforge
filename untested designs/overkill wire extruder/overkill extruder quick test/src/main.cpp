f7011e140e1e665e8cab55bc5f387c7def396f5b
Avery Lockwood <bmlockwoodjohnston@outlook.com>
2024-11-23 01:04:22 -0800

    added top wire support for overkill extruder

--- untested designs/overkill wire extruder/overkill extruder quick test/src/main.cpp
+++ untested designs/overkill wire extruder/overkill extruder quick test/src/main.cpp
@@ -5,6 +5,12 @@ const int PULSE = 12;
 const int DIR = 13;
 const int ENA = 11;
 const int DUTY_CYCLE = 50;
+const int POTPIN = 0;
+const int BUTTONPIN = 1;
+const int STEPPERPOT = 2;
+
+int stepspeed;
+int speed = 0;
 Servo ESC;
 
 
@@ -33,19 +39,32 @@ void setup() {
 pinMode(PULSE, OUTPUT);
 pinMode(DIR, OUTPUT);
 pinMode(ENA, OUTPUT);
+pinMode(POTPIN,INPUT);
+pinMode(BUTTONPIN, INPUT);
+pinMode(STEPPERPOT ,INPUT);
 digitalWrite(ENA, LOW);
+
 Serial.begin(9600);
 ESC.attach(7,1000,2000);
-//pinMode(7,OUTPUT);
+
+
 }
 
 
 void loop() {
-//move(100, 3000);
-///delay(100);
-ESC.write(90);
-delay(4000);
-ESC.write(180); 
-delay(5000);
+speed = analogRead(POTPIN);
+speed = map(speed, 0, 1023, 0, 180);
+ESC.write(speed);
+Serial.print("ervoSpeed:");
+Serial.println(speed);
+
+stepspeed = analogRead(STEPPERPOT);
+Serial.print("stepspeed");
+Serial.println(stepspeed);
+
+if(analogRead(BUTTONPIN) > 1022){
+move(stepspeed, 500);
+}
+
 
 }
